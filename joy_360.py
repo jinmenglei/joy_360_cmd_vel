20ddf651e6f65a49ca8d2a6c91921de8e0364b00
jinmenglei <229143434@qq.com>
2020-09-20 22:55:24 +0800

    update

--- joy_360.py
+++ joy_360.py
@@ -2,6 +2,7 @@ import time
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from can_msgs.msg import Frame
 
 
 class Joy360:
@@ -9,12 +10,23 @@ class Joy360:
         self.__line_x = 0
         self.__angle_z = 0
         self.__pub_cmd_vel = None #:
+        self.__pub_can_vel = None
         self.__start = False
         pass
 
     def joy_callback(self,data:Joy):
         if data.buttons[7] == 1:
             self.__start = not self.__start
+            send_data = Frame()
+            send_data.dlc = 8
+            send_data.id = 2
+            send_data.data = [0x00, 0xDA, 0x00, 0x10, 0x00, 0x00, 0x00, 0x1F]
+            if not self.__start:
+                send_data.data[7] = 0x0F
+            self.__pub_can_vel.publish(send_data)
+            send_data.id = 3
+            self.__pub_can_vel.publish(send_data)
+
         # rospy.loginfo(str(data.axes))
         self.__line_x = data.axes[4]
         self.__angle_z = data.axes[0]
@@ -25,6 +37,7 @@ class Joy360:
         rospy.loginfo("init node ok!!")
         rospy.Subscriber("/joy",Joy,self.joy_callback)
         self.__pub_cmd_vel = rospy.Publisher("/cmd_vel",Twist,queue_size=100)
+        self.__pub_can_vel = rospy.Publisher("/sent_messages",Frame,queue_size=100)
 
         try:
             while not rospy.is_shutdown():
