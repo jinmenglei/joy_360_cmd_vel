7a2f9865dcc86e3b4f1c4b109a782283585f90d1
229143434@qq.com <zxcvbnm123-*@>
2020-09-21 00:25:44 +0800

    xxxx

--- cmd_2_can.py
+++ cmd_2_can.py
@@ -9,30 +9,46 @@ class Cmd2can:
     def __init__(self):
         self.__line_x = 0
         self.__angle_z = 0
-        self.__pub_cmd_vel = None #:
+        self.__pub_can_vel = None #:
         self.__start = False
         pass
 
     def cmd_callback(self,data:Twist):
         x_speed=data.linear.x
         z_speed=data.angular.z
-        print(x_speed)
-        print(z_speed)
-        print("------")
+
         l_speed = (x_speed+z_speed)/2
         r_speed = (x_speed-z_speed)/2
-        print(l_speed)
-        print(r_speed)
-        print("------")
+
         l_turn = l_speed/PERC
         r_turn = r_speed/PERC
-        print(l_turn)
-        print(r_turn)
-        print("------")
+
         l_turn = l_turn*8192/3000 *60
         r_turn = r_turn * 8192 / 3000 *60
-        print(l_turn)
-        print(hex(int(l_turn)))
+
+        l_turn_data_byte = int(l_turn).to_bytes(4, byteorder='big', signed=True)
+        r_turn_data_byte = int(r_turn).to_bytes(4, byteorder='big', signed=True)
+
+        send_data = Frame()
+        send_data.dlc = 8
+        send_data.id = 2
+        send_data.data = [0x00, 0xDA, 0x00, 0x11, 0x00, 0x00, 0x00, 0x00]
+
+        send_data.data[4] = l_turn_data_byte[0]
+        send_data.data[5] = l_turn_data_byte[1]
+        send_data.data[6] = l_turn_data_byte[2]
+        send_data.data[7] = l_turn_data_byte[3]
+
+        self.__pub_can_vel.publish(send_data)
+
+        send_data.id = 3
+        send_data.data[4] = r_turn_data_byte[0]
+        send_data.data[5] = r_turn_data_byte[1]
+        send_data.data[6] = r_turn_data_byte[2]
+        send_data.data[7] = r_turn_data_byte[3]
+
+        self.__pub_can_vel.publish(send_data)
+
 
 
 
@@ -41,7 +57,7 @@ class Cmd2can:
         rospy.init_node("cmd_vel_2_can", disable_signals=True)
         rospy.loginfo("init node ok!!")
         rospy.Subscriber("/cmd_vel",Twist,self.cmd_callback)
-        self.__pub_cmd_vel = rospy.Publisher("/sent_messages",Frame,queue_size=100)
+        self.__pub_can_vel = rospy.Publisher("/sent_messages",Frame,queue_size=100)
 
         try:
             while not rospy.is_shutdown():
@@ -52,5 +68,9 @@ class Cmd2can:
 
 
 if __name__=="__main__":
+    # data = -100
+    # data_byte = data.to_bytes(4, byteorder='big', signed=True)
+    # print(data_byte[1])
+    # exit(0)
     cmd_ins = Cmd2can()
     cmd_ins.start()
