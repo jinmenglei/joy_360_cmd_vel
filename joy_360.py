1ef1a07c993b0c45f8041450fb44a243b2550e55
jinmenglei <229143434@qq.com>
2020-11-30 11:35:59 +0800

    hha

--- joy_360.py
+++ joy_360.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+#coding=utf-8
 import time
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-from can_msgs.msg import Frame
+#from can_msgs.msg import Frame
 
 
 class Joy360:
@@ -15,21 +17,21 @@ class Joy360:
         pass
 
     def joy_callback(self,data:Joy):
-        if data.buttons[11] == 1:
+        if data.buttons[7] == 1:
             self.__start = not self.__start
-            send_data = Frame()
-            send_data.dlc = 8
-            send_data.id = 2
-            send_data.data = [0x00, 0xDA, 0x00, 0x10, 0x00, 0x00, 0x00, 0x1F]
-            if not self.__start:
-                send_data.data[7] = 0x0F
-            self.__pub_can_vel.publish(send_data)
-            send_data.id = 3
-            self.__pub_can_vel.publish(send_data)
+ #           send_data = Frame()
+ #           send_data.dlc = 8
+ #           send_data.id = 2
+ #           send_data.data = [0x00, 0xDA, 0x00, 0x10, 0x00, 0x00, 0x00, 0x1F]
+ #           if not self.__start:
+ #               send_data.data[7] = 0x0F
+ #           self.__pub_can_vel.publish(send_data)
+ #           send_data.id = 3
+ #           self.__pub_can_vel.publish(send_data)
 
         # rospy.loginfo(str(data.axes))
-        self.__line_x = data.axes[3]
-        self.__angle_z = data.axes[0]
+        self.__line_x = data.axes[1]
+        self.__angle_z = data.axes[3]
 
     def start(self):
         print("start init this node")
@@ -37,7 +39,7 @@ class Joy360:
         rospy.loginfo("init node ok!!")
         rospy.Subscriber("/joy",Joy,self.joy_callback)
         self.__pub_cmd_vel = rospy.Publisher("/cmd_vel",Twist,queue_size=100)
-        self.__pub_can_vel = rospy.Publisher("/sent_messages",Frame,queue_size=100)
+#        self.__pub_can_vel = rospy.Publisher("/sent_messages",Frame,queue_size=100)
 
         try:
             while not rospy.is_shutdown():
